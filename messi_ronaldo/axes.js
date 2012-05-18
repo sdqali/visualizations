5282399d40592f81a686db8eb550dbe6f1f6ccf5
Sadique Ali <sadiqalikm@gmail.com>
2012-05-18 21:17:31 +0530

    Using the builder for axes.

--- messi_ronaldo/axes.js
+++ messi_ronaldo/axes.js
@@ -1,106 +1,106 @@
 var Axis = function(svg) {
-	this.svg = svg;
-	this.padding = 30;
-	this.height = 500;
-	this.width = 1000;
-	this.xLabelText = "X Label";
-	this.yLabelText = "Y Label";
+    this.svg = svg;
+    this.padding = 30;
+    this.height = 500;
+    this.width = 1000;
+    this.xLabelText = "X Label";
+    this.yLabelText = "Y Label";
 };
 
 Axis.prototype.withPadding = function(padding) {
-	this.pading = padding;
-	return this;
+    this.pading = padding;
+    return this;
 };
 
 Axis.prototype.withHeight = function(height) {
-	this.height = height;
-	return this;
+    this.height = height;
+    return this;
 };
 
 Axis.prototype.withWidth = function(width) {
-	this.width = width;
-	return this;
+    this.width = width;
+    return this;
 };
 
 Axis.prototype.withXLabelText = function(xLabelText) {
-	this.xLabelText = xLabelText;
-	return this;
+    this.xLabelText = xLabelText;
+    return this;
 };
 
 Axis.prototype.withYLabelText = function(yLabelText) {
-	this.yLabelText = yLabelText;
-	return this;
+    this.yLabelText = yLabelText;
+    return this;
 };
 
 Axis.prototype.withXScale = function(xScale) {
-	this.xScale = xScale;
-	return this;
+    this.xScale = xScale;
+    return this;
 };
 
 Axis.prototype.withYScale = function(yScale) {
-	this.yScale = yScale;
-	return this;
+    this.yScale = yScale;
+    return this;
 };
 
 Axis.prototype.draw = function() {
-	this.height = this.svg.attr("height");
-	this.width = this.svg.attr("width");
-	var self = this;
+    this.height = this.svg.attr("height");
+    this.width = this.svg.attr("width");
+    var self = this;
 
-	var xAxis = d3.svg.axis()
+    var xAxis = d3.svg.axis()
 	.scale(this.xScale)
 	.ticks(20)
 	.orient("bottom");
 
-	this.svg.append("g")
+    this.svg.append("g")
 	.attr("class", "axis")
 	.attr("transform", "translate(0," + (this.height - 2 * this.padding) + ")")
 	.call(xAxis);
 
-	this.svg.append("text")
+    this.svg.append("text")
 	.text(this.xLabelText)
 	.attr("x", (this.width - 2 * this.padding)/2)
 	.attr("y", this.height - this.padding);
 
-	var yAxis = d3.svg.axis()
+    var yAxis = d3.svg.axis()
 	.scale(this.yScale)
 	.ticks(20)
 	.orient("left");
 
-	this.svg.append("g")
+    this.svg.append("g")
 	.attr("class", "axis")
 	.attr("transform", "translate(" +  2 * this.padding + ", 0)")
 	.call(yAxis);
 
-	var yLabel = this.svg.append("g");
-	yLabel.append("text")
+    var yLabel = this.svg.append("g");
+    yLabel.append("text")
 	.text(this.yLabelText)
 	.attr("text-anchor", "middle");
-	yLabel.attr("transform", "translate(" + this.padding/2 + "," +  (this.height - this.padding)/2 +") rotate(270)");
+    yLabel.attr("transform", "translate(" + this.padding/2 + "," +  (this.height - this.padding)/2 +") rotate(270)");
 
-	var xRule = this.svg.append("g")
+    var xRule = this.svg.append("g")
 	.selectAll(".rule")
 	.data(this.yScale.ticks(20))
 	.enter()
 	.append("g")
 	.attr("class", "rule")
 	.attr("transform", function(d) {
-		return "translate(" + 2 * self.padding + ", " + self.yScale(d) + ")";
-	});
+		  return "translate(" + 2 * self.padding + ", " + self.yScale(d) + ")";
+	      });
 
-	xRule.append("line")
+    xRule.append("line")
 	.attr("x2", this.width - 3 * this.padding);
 
-	var yRule = this.svg.append("g")
+    var yRule = this.svg.append("g")
 	.selectAll(".rule")
 	.data(this.xScale.ticks(20))
 	.enter()
 	.append("g")
 	.attr("class", "rule")
 	.attr("transform", function(d) {
-		return "translate(" + self.xScale(d) + "," + self.padding + ")";
-	});
+		  return "translate(" + self.xScale(d) + "," + self.padding + ")";
+	      });
 
-	yRule.append("line")
+    yRule.append("line")
 	.attr("y2", h - 3 * this.padding);
 };
--- messi_ronaldo/timeseries.js
+++ messi_ronaldo/timeseries.js
@@ -2,16 +2,16 @@ var w = 1000;
 var h = 600;
 var padding = 30;
 var colorMap = {
-	"Cristiano Ronaldo": "#1f77b4",
-	"Lionel Messi": "#637939"
+    "Cristiano Ronaldo": "#1f77b4",
+    "Lionel Messi": "#637939"
 };
 
 var Timeseries = function() {};
 
 
 Timeseries.prototype.draw = function(className) {
-	var vis = createSkelton(className);
-	d3.json("data/processed_data.json", function(players) {
+    var vis = createSkelton(className);
+    d3.json("data/processed_data.json", function(players) {
 		var maxGames = maxGamesAmong(players);
 		var maxGoals = maxGoalsAmong(players);
 
@@ -22,68 +22,74 @@ Timeseries.prototype.draw = function(className) {
 		var radiusScale = d3.scale.linear().domain([0, maxGoals]).range([1, maxGoals]);
 
 		players.forEach(function(player) {
-			var performances = sanitize(player.performances);
-			var circles = vis.append("g").selectAll("circle").data(performances).enter().append("circle").attr("cx", function(d, i) {
-				return xAxisScale(i);
-			}).attr("cy", function(d, i) {
-				var ps = performances.slice(0, i + 1);
-				var val = ps.reduce(function(a, b) {
-					return {
-						goals: a.goals + b.goals
-					}
-				}).goals;
-				return yAxisScale(val);
-			}).attr("r", 2).attr("fill", colorMap[player.name]);
+				    var performances = sanitize(player.performances);
+				    var circles = vis.append("g")
+					.selectAll("circle")
+					.data(performances)
+					.enter()
+					.append("circle")
+					.attr("cx", function(d, i) {
+						  return xAxisScale(i);
+					      }).attr("cy", function(d, i) {
+							  var ps = performances.slice(0, i + 1);
+							  var val = ps.reduce(function(a, b) {
+										  return {
+										      goals: a.goals + b.goals
+										  };
+									      }).goals;
+							  return yAxisScale(val);
+						      }).attr("r", 2).attr("fill", colorMap[player.name]);
 
-			circles.append("title").text(function(d) {
-				return prettyText(d)
-			});
-			var axes = new Axis(vis)
-			.withPadding(padding)
-			.withXScale(xAxisScale)
-			.withYScale(yAxisScale)
-			.withXLabelText("Games")
-			.withYLabelText("Cumulative Goals");
-			axes.draw();
-		});
+				    circles.append("title").text(function(d) {
+								     return prettyText(d);
+								 });
 
-	});
+				    var axes = new Axis(vis)
+					.withPadding(padding)
+					.withXScale(xAxisScale)
+					.withYScale(yAxisScale)
+					.withXLabelText("Games")
+					.withYLabelText("Cumulative Goals");
+				    axes.draw();
+				});
+
+	    });
 };
 
 function maxGoalsAmong(players) {
-	var goals = players.map(function(p) {
-		return totalGoalsBy(p);
-	});
-	return d3.max(goals, function(d) {
-		return d;
-	});
+    var goals = players.map(function(p) {
+				return totalGoalsBy(p);
+			    });
+    return d3.max(goals, function(d) {
+		      return d;
+		  });
 };
 
 function maxGamesAmong(players) {
-	var games = players.map(function(p) {
-		return p.performances.length
-	});
-	return d3.max(games, function(d) {
-		return d;
-	});
+    var games = players.map(function(p) {
+				return p.performances.length;
+			    });
+    return d3.max(games, function(d) {
+		      return d;
+		  });
 };
 
 function totalGoalsBy(player) {
-	return d3.sum(player.performances.map(function(a) {
-		return a.goals;
-	}));
+    return d3.sum(player.performances.map(function(a) {
+					      return a.goals;
+					  }));
 }
 
 function createSkelton(className) {
-	return d3.select("." + className).append("svg").attr("width", w).attr("height", h);
+    return d3.select("." + className).append("svg").attr("width", w).attr("height", h);
 };
 
 function sanitize(performances) {
-	return performances.filter(function(d) {
-		return d.goals != null;
-	});
+    return performances.filter(function(d) {
+				   return d.goals != null;
+			       });
 };
 
 function prettyText(p) {
-	return [p.date, p.opponent, p.competition].join(", ") + " Goals: " + p.goals + ",Assists: " + p.assists;
+    return [p.date, p.opponent, p.competition].join(", ") + " Goals: " + p.goals + ",Assists: " + p.assists;
 };
